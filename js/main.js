979f10262379ec6729d5beb9d4cdb49e8fc2c683
Mtutyunjay Madarimath <88146245+JayMadari@users.noreply.github.com>
2022-05-21 18:37:56 +0530

    commit on master

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-// Popovers
-
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-
-// Select gender
-
-if(window.location.pathname === '/'){
-    const radioBtn1 = document.querySelector('#male')
-    const radioBtn2 = document.querySelector('#female')
-    const radioBtn3 = document.querySelector('#custom')
-    const genderSelect = document.querySelector('#genderSelect')
-
-    radioBtn1.addEventListener("change", ()=>{
-        genderSelect.classList.add('d-none')
-    })
-    radioBtn2.addEventListener("change", ()=>{
-        genderSelect.classList.add('d-none')
-    })
-    radioBtn3.addEventListener("change", ()=>{
-        genderSelect.classList.remove('d-none')
-    })
-
-}
+  return new bootstrap.Popover(popoverTriggerEl);
+});
